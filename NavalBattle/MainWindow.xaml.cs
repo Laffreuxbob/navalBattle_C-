8a69c79212f90f8f351dc82cbaf2f05312b9c4ab
Unknown <olivier.bricaud@sfr.fr>
2019-02-06 13:58:18 +0100

    add regiond to MainWindow.cs, first test creating box

--- NavalBattle/MainWindow.xaml.cs
+++ NavalBattle/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NavalBattle.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace NavalBattle
 {
     /// <summary>
@@ -20,9 +22,52 @@ namespace NavalBattle
     /// </summary>
     public partial class MainWindow : Window
     {
+
+
+        #region StaticVariables
+        #endregion
+
+        #region Constants
+        #endregion
+
+        #region Variables
+        #endregion
+
+        #region Attributs
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
+            createBox();
+
         }
+        #endregion
+
+        #region StaticFunctions
+        
+        #endregion
+
+        #region Functions
+
+        public void createBox()
+        {
+            System.Console.WriteLine("create box");
+            Box box1 = new Box();
+            box1.show();
+            
+        }
+        #endregion
+
+        #region Events
+        #endregion
+
     }
 }
