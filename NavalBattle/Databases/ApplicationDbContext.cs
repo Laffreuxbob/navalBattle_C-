9533946f85f3f2343f29e55bc6a8ba682da10660
Yannoël <yannoel.martin@hotmail.fr>
2019-02-06 13:59:13 +0100

    préparation de l'élément pour la bdd

---
+++ NavalBattle/Databases/ApplicationDbContext.cs
@@ -0,0 +1,50 @@
+﻿using NavalBattle.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NavalBattle.Databases
+{
+    public class ApplicationDbContext
+    {
+
+        #region StaticVariables
+        #endregion
+
+        #region Constants
+        #endregion
+
+        #region Variables
+        #endregion
+
+        #region Attributs
+
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public ApplicationDbContext()
+        {
+                
+        }
+        #endregion
+
+        #region StaticFunctions
+        #endregion
+
+        #region Functions
+        #endregion
+
+        #region Events
+        #endregion
+
+
+    }
+}
