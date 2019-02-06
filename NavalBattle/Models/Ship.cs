783a7243f212ab3c7dc41c1ef6421f4188a8085a
Yannoël <yannoel.martin@hotmail.fr>
2019-02-06 14:46:17 +0100

    fonction & import siip

--- NavalBattle/Models/Ship.cs
+++ NavalBattle/Models/Ship.cs
@@ -1,5 +1,6 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -84,6 +85,34 @@ namespace NavalBattle.Models
         #endregion
 
         #region Functions
+        public void ShipCreation()
+        {
+            // first ship
+            Ship shipA = new Ship();
+            shipA.Id = 1;
+            shipA.Name = "Submarine";
+            shipA.NbBox = 2;
+            shipA.State = true;
+            shipA.PicturePath = "";
+            Ship shipB = new Ship();
+            shipB.Id = 2;
+            shipB.Name = "Corvette";
+            shipB.NbBox = 3;
+            shipB.State = true;
+            shipB.PicturePath = "";
+            Ship shipC = new Ship();
+            shipC.Id = 3;
+            shipC.Name = "Cruiser";
+            shipC.NbBox = 4;
+            shipC.State = true;
+            shipC.PicturePath = "";
+            Ship shipD = new Ship();
+            shipD.Id = 4;
+            shipD.Name = "Aircraft carrier";
+            shipD.NbBox = 5;
+            shipD.State = true;
+            shipD.PicturePath = "";
+        }
         #endregion
 
         #region Events
