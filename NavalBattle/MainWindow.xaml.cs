6ee12239a0ac57eb07cc2c8731df1c0ed3f96e93
Yannoël <yannoel.martin@hotmail.fr>
2019-02-06 16:40:10 +0100

    maj fonctions et algo

--- NavalBattle/MainWindow.xaml.cs
+++ NavalBattle/MainWindow.xaml.cs
@@ -52,7 +52,36 @@ namespace NavalBattle
         {
             InitializeComponent();
             createBox();
+            list_ship_display = Ship.ShipCreation();
+            placementPlayeur.Clear();
+            placementVersus.Clear();
+            foreach (Ship item in list_ship_display)
+            {
+                // define quantity, name and dimension for each ship
 
+                // affichage similaire pour chaque navire -> template
+                // attente de l'évènement (click) qui fait passer a la suite
+                quantity = 1;
+                ListShip element = new ListShip();
+                element.Quantity = quantity;
+                element.Chaine = quantity + " " + item.Name + " restant(s).";
+
+                maListe.Clear();
+                for (int i = 0; i < quantity; i++)
+                {
+                    Ship boat = new Ship(item.Name, item.WidthNbBox, item.HeightNbBox, item.State, item.PicturePath);
+                    maListe.Add(boat);
+                }
+                element.ShipsList = maListe;
+
+                placementPlayeur.Add(element);
+                placementVersus.Add(element);
+            }
+            // define position for each ship of the playeur
+            // define position for each ship of the IA
+            /* begin to delete when position is operationnal */
+            /* end to delete when position is operationnal */
+            display_ship_list();
         }
         #endregion
 
@@ -68,6 +97,12 @@ namespace NavalBattle
             Box box1 = new Box();
             box1.show();
             
+        }
+
+        public void display_ship_list()
+        {
+            String elem_display_playeur = "";
+            String elem_display_versus = "";
         }
         #endregion
 
