45d25993e1edf2b98031c59c906db2a7c2c1ef3e
Yannoël <yannoel.martin@hotmail.fr>
2019-02-07 14:58:36 +0100

    ajout de la page pour sélectionner les informations du second navire

---
+++ NavalBattle/Views/PageSecondShipChoice.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NavalBattle.Views
+{
+    /// <summary>
+    /// Logique d'interaction pour PageFirstShipChoice.xaml
+    /// </summary>
+    public partial class PageSecondShipChoice : Page
+    {
+
+
+        #region StaticVariables
+        #endregion
+
+        #region Constants
+        #endregion
+
+        #region Variables
+        #endregion
+
+        #region Attributs
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public PageSecondShipChoice()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region StaticFunctions
+        #endregion
+
+        #region Functions
+        #endregion
+
+        #region Events
+        private void secondShipChoice_Click(object sender, RoutedEventArgs e)
+        {
+            (this.Parent as Window).Content = new PageThirdShipChoice();
+        }
+        #endregion
+
+    }
+}
