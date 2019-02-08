d187d99b42e549db2e7cc8aa29e15c9e07f09002
Yannoël <yannoel.martin@hotmail.fr>
2019-02-08 14:05:50 +0100

    maj algo first ship

--- NavalBattle/Views/PageFirstShipChoice.xaml.cs
+++ NavalBattle/Views/PageFirstShipChoice.xaml.cs
@@ -56,11 +56,14 @@ namespace NavalBattle.Views
         #region Events
         private void firstShipChoice_Click(object sender, RoutedEventArgs e)
         {
+            ListShip listReturn = new ListShip();
+            List<Ship> submarineList = new List<Ship>();
             Ship submarine = new Ship();
             submarine.Name = "Submarine";
             submarine.State = true;
             submarine.PicturePath = "pack://application:,,,/NavalBattle;component/Resources/submarine.jpg";
 
+            // dimensions of the ship
             if (this.firstShipWidthTxt.Text == "" || this.firstShipWidthTxt.Text == "0")
             {
                 submarine.WidthNbBox = 2;
@@ -74,7 +77,6 @@ namespace NavalBattle.Views
                 }
                 submarine.WidthNbBox = widthChoice;
             }
-
             if (this.firstShipHeightTxt.Text == "" || this.firstShipHeightTxt.Text == "0")
             {
                 submarine.HeightNbBox = 1;
@@ -86,11 +88,42 @@ namespace NavalBattle.Views
                 {
                     heightChoice = -heightChoice;
                 }
-                submarine.WidthNbBox = heightChoice;
+                submarine.HeightNbBox = heightChoice;
+            }
+            submarine.PositionShip = new int[submarine.WidthNbBox, submarine.HeightNbBox];
+
+            // number of ship
+            int quantity = 0;
+            if (this.firstShipQuantityTxt.Text == "" || this.firstShipQuantityTxt.Text == "0")
+            {
+                quantity = 1;
+            }
+            else
+            {
+                quantity = int.Parse(this.firstShipHeightTxt.Text);
+                if (quantity < 0)
+                {
+                    quantity = -quantity;
+                }
+
+                // gestion de la quantity a finalisée
+
             }
 
-            // gestion de la quantity
-            // enregistrement du bateau dans une list epour affichage ?
+            listReturn.Quantity = quantity;
+            listReturn.QuantityAlive = quantity;
+            listReturn.DisplayString = listReturn.QuantityAlive + " " + submarine.Name + " alive";
+            for (int i = 0; i < quantity; i++)
+            {
+                submarineList.Add(submarine);
+            }
+            listReturn.ShipsList = submarineList;
+
+            (this.Parent as MainWindow).PlacementPlayer = new List<ListShip>();
+            (this.Parent as MainWindow).PlacementVersus = new List<ListShip>();
+
+            (this.Parent as MainWindow).PlacementPlayer.Add(listReturn);
+            (this.Parent as MainWindow).PlacementVersus.Add(listReturn);
 
             (this.Parent as Window).Content = new PageSecondShipChoice();
         }
